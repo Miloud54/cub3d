0c6fff58a17b06abf8f0faa8090447d80ab8dfd5
Brunhilde De La Prada <bde-la-p@f1br9s2.clusters.42paris.fr>
2025-11-19 15:17:04 +0100

    upgraded check_expansion function

--- srcs/parsing/check_extension.c
+++ srcs/parsing/check_extension.c
@@ -6,7 +6,7 @@
 /*   By: bde-la-p <bde-la-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/19 13:51:32 by bde-la-p          #+#    #+#             */
-/*   Updated: 2025/11/19 13:52:08 by bde-la-p         ###   ########.fr       */
+/*   Updated: 2025/11/19 15:12:24 by bde-la-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,27 @@
 
 int	is_valid_extension(char *filename)
 {
+	char	*dot_pos;
+	char	*first_dot;
 	int		len;
 
 	len = ft_strlen(filename);
-	if (len < 4)
+	if (!filename || len < 4)
 	{
-		write(2, "Error\nWrong file extension\n", 27);
+		write(2, "Error\nInvalid filename\n", 23);
 		return (0);
 	}
-	if (filename[len - 1] == 'b' && filename[len - 2] == 'u'
-		&& filename[len - 3] == 'c' && filename[len - 4] == '.')
-		return (1);
-	write(2, "Error\nWrong file extension\n", 27);
-	return (0);
+	if (ft_strcmp(filename + len - 4, ".cub") != 0)
+	{
+		write(2, "Error\nWrong file extension (must be .cub)\n", 42);
+		return (0);
+	}
+	first_dot = ft_strchr(filename, '.');
+	dot_pos = ft_strrchr(filename, '.');
+	if (first_dot != dot_pos)
+	{
+		write(2, "Error\nMultiple extensions not allowed\n", 38);
+		return (0);
+	}
+	return (1);
 }
