d686d9d5b3ed0c85ca19b94e8f76020a15352288
Emilie Didier <edidier@f1r2s4.clusters.42paris.fr>
2025-11-21 17:16:16 +0100

    amelioration file_exists

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: edidier <edidier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 14:21:24 by edidier           #+#    #+#             */
-/*   Updated: 2025/11/21 16:31:01 by edidier          ###   ########.fr       */
+/*   Updated: 2025/11/21 17:13:54 by edidier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,8 @@
 # include <stdlib.h>
 # include <string.h>
 # include <unistd.h>
+# include <errno.h>
+
 
 # define TILE_SIZE 64
 # define MAX_MAP_WIDTH 32
