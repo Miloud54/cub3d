05b2d6f65b60c969f02cefb6bcb01e3aa1bf9c47
Brunhilde De La Prada <bde-la-p@f1r8s20.clusters.42paris.fr>
2025-11-25 18:01:47 +0100

    added bonus rule to mouse for pov

--- srcs/handle_input.c
+++ srcs/handle_input.c
@@ -6,7 +6,7 @@
 /*   By: bde-la-p <bde-la-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/19 13:56:02 by bde-la-p          #+#    #+#             */
-/*   Updated: 2025/11/25 15:28:56 by bde-la-p         ###   ########.fr       */
+/*   Updated: 2025/11/25 18:01:23 by bde-la-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,7 +64,6 @@ int	key_release(int keycode, t_game *game)
 	return (0);
 }
 
-// Mouse look function for camera control
 void	mouse_look(t_game *game)
 {
 	int		current_x;
@@ -79,32 +78,36 @@ void	mouse_look(t_game *game)
 	if (delta_x != 0)
 	{
 		rotation_angle = delta_x * MOUSE_SENSITIVITY;
-		game->player.dir_x = game->player.dir_x * cos(rotation_angle) - game->player.dir_y * sin(rotation_angle);
-		game->player.dir_y = game->player.dir_x * sin(rotation_angle) + game->player.dir_y * cos(rotation_angle);
-		game->player.plane_x = game->player.plane_x * cos(rotation_angle) - game->player.plane_y * sin(rotation_angle);
-		game->player.plane_y = game->player.plane_x * sin(rotation_angle) + game->player.plane_y * cos(rotation_angle);
+		game->player.dir_x = game->player.dir_x * cos(rotation_angle)
+			- game->player.dir_y * sin(rotation_angle);
+		game->player.dir_y = game->player.dir_x * sin(rotation_angle)
+			+ game->player.dir_y * cos(rotation_angle);
+		game->player.plane_x = game->player.plane_x * cos(rotation_angle)
+			- game->player.plane_y * sin(rotation_angle);
+		game->player.plane_y = game->player.plane_x * sin(rotation_angle)
+			+ game->player.plane_y * cos(rotation_angle);
 	}
 	game->last_mouse_x = current_x;
-	// Keep mouse centered for continuous rotation
 	if (current_x < 200 || current_x > game->win_w - 200)
 	{
-		mlx_mouse_move(game->mlx, game->window, game->win_w / 2, game->win_h / 2);
+		mlx_mouse_move(game->mlx, game->window, game->win_w / 2, game->win_h
+			/ 2);
 		game->last_mouse_x = game->win_w / 2;
 	}
 }
 
-// Mouse click handler
 int	mouse_hook(int button, int x, int y, t_game *game)
 {
 	(void)x;
 	(void)y;
-	if (button == 1) // Left click toggles mouse look
+	if (button == 1)
 	{
 		game->mouse_enabled = !game->mouse_enabled;
 		if (game->mouse_enabled)
 		{
 			mlx_mouse_hide(game->mlx, game->window);
-			mlx_mouse_move(game->mlx, game->window, game->win_w / 2, game->win_h / 2);
+			mlx_mouse_move(game->mlx, game->window, game->win_w / 2, game->win_h
+				/ 2);
 			game->last_mouse_x = game->win_w / 2;
 		}
 		else
@@ -116,5 +119,6 @@ int	mouse_hook(int button, int x, int y, t_game *game)
 void	handle_input(t_game *game)
 {
 	process_input(game);
-	mouse_look(game);
+	if (BONUS)
+		mouse_look(game);
 }
