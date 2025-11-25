1c680ccd544c91c0e967c73f0379ac30c0e14d16
Brunhilde De La Prada <bde-la-p@f1r8s20.clusters.42paris.fr>
2025-11-25 18:21:44 +0100

    norminetted map_init.c and check_extension.c

--- srcs/map_init.c
+++ srcs/map_init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   map_init.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: edidier <edidier@student.42.fr>            +#+  +:+       +#+        */
+/*   By: bde-la-p <bde-la-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/19 12:24:58 by bde-la-p          #+#    #+#             */
-/*   Updated: 2025/11/24 16:34:00 by edidier          ###   ########.fr       */
+/*   Updated: 2025/11/25 18:16:50 by bde-la-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,6 @@ static int	init_window(t_game *game)
 	int	width;
 	int	height;
 
-	// Fixed window size for optimal performance
 	width = WINDOW_WIDTH;
 	height = WINDOW_HEIGHT;
 	game->window = mlx_new_window(game->mlx, width, height, "cub3D");
--- srcs/parsing/check_extension.c
+++ srcs/parsing/check_extension.c
@@ -6,7 +6,7 @@
 /*   By: bde-la-p <bde-la-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/19 13:51:32 by bde-la-p          #+#    #+#             */
-/*   Updated: 2025/11/21 17:16:00 by bde-la-p         ###   ########.fr       */
+/*   Updated: 2025/11/25 18:20:30 by bde-la-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,14 +38,9 @@ int	is_valid_extension(char *filename)
 	}
 	return (1);
 }
-int	is_valid_extension_xpm(char *filename)
-{
-	char	*dot_pos;
-	char	*basename_start;
-	char	*first_dot_in_name;
-	int		len;
 
-	len = ft_strlen(filename);
+static int	check_xpm_extension_validity(char *filename, int len)
+{
 	if (!filename || len < 4)
 	{
 		write(2, "Error\nInvalid filename\n", 23);
@@ -56,13 +51,20 @@ int	is_valid_extension_xpm(char *filename)
 		write(2, "Error\nWrong file extension (must be .xpm)\n", 42);
 		return (0);
 	}
-	// Trouver le début du nom de fichier (après le dernier '/')
+	return (1);
+}
+
+static int	check_xpm_multiple_extensions(char *filename)
+{
+	char	*dot_pos;
+	char	*basename_start;
+	char	*first_dot_in_name;
+
 	basename_start = ft_strrchr(filename, '/');
 	if (basename_start)
 		basename_start++;
 	else
 		basename_start = filename;
-	// Vérifier les extensions multiples seulement dans le nom de fichier
 	first_dot_in_name = ft_strchr(basename_start, '.');
 	dot_pos = ft_strrchr(basename_start, '.');
 	if (first_dot_in_name != dot_pos)
@@ -70,5 +72,17 @@ int	is_valid_extension_xpm(char *filename)
 		write(2, "Error\nMultiple extensions not allowed\n", 38);
 		return (0);
 	}
+	return (1);
+}
+
+int	is_valid_extension_xpm(char *filename)
+{
+	int	len;
+
+	len = ft_strlen(filename);
+	if (!check_xpm_extension_validity(filename, len))
+		return (0);
+	if (!check_xpm_multiple_extensions(filename))
+		return (0);
 	return (1);
 }
