9d4fa9d1b2a1bdfa081f9c0888c4a921dab8e779
Brunhilde De La Prada <bde-la-p@f4r6s2.clusters.42paris.fr>
2025-11-28 16:27:04 +0100

    main.c is now norminetted too

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: edidier <edidier@student.42.fr>            +#+  +:+       +#+        */
+/*   By: bde-la-p <bde-la-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/19 12:08:28 by bde-la-p          #+#    #+#             */
-/*   Updated: 2025/11/28 13:56:48 by edidier          ###   ########.fr       */
+/*   Updated: 2025/11/28 16:24:45 by bde-la-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,43 @@ static int	validate_args(int ac, char **av)
 	return (1);
 }
 
+static int	setup_parsing(char *filename, t_game *game)
+{
+	if (!parse_scene(filename, game) || !validate_map_structure(game)
+		|| !validate_textures(&game->textures)
+		|| !extract_player_position(game) || !extract_enemies(game))
+	{
+		cleanup_game(game);
+		return (0);
+	}
+	return (1);
+}
+
+static int	setup_game(t_game *game)
+{
+	if (!init_game(game))
+	{
+		cleanup_game(game);
+		return (0);
+	}
+	if (!load_textures(game))
+	{
+		cleanup_game(game);
+		return (0);
+	}
+	return (1);
+}
+
+static void	setup_hooks(t_game *game)
+{
+	mlx_hook(game->window, 2, 1L << 0, key_press, game);
+	mlx_hook(game->window, 3, 1L << 1, key_release, game);
+	mlx_mouse_hook(game->window, mouse_hook, game);
+	mlx_loop_hook(game->mlx, render_loop, game);
+	mlx_expose_hook(game->window, render_loop, game);
+	mlx_hook(game->window, 17, 0, close_game, game);
+}
+
 int	main(int ac, char **av)
 {
 	t_game	game;
@@ -33,33 +70,12 @@ int	main(int ac, char **av)
 	ft_memset(&game, 0, sizeof(t_game));
 	if (!validate_args(ac, av))
 		return (1);
-	if (!parse_scene(av[1], &game) || !validate_map_structure(&game)
-		|| !validate_textures(&game.textures) || !extract_player_position(&game)
-#if BONUS
-		|| !extract_enemies(&game)
-#endif
-	)
-	{
-		cleanup_game(&game);
+	if (!setup_parsing(av[1], &game))
 		return (1);
-	}
-	if (!init_game(&game))
-	{
-		cleanup_game(&game);
+	if (!setup_game(&game))
 		return (1);
-	}
-	if (!load_textures(&game))
-	{
-		cleanup_game(&game);
-		return (1);
-	}
-	mlx_hook(game.window, 2, 1L << 0, key_press, &game);
-	mlx_hook(game.window, 3, 1L << 1, key_release, &game);
-	mlx_mouse_hook(game.window, mouse_hook, &game);
-	mlx_loop_hook(game.mlx, render_loop, &game);
-	mlx_expose_hook(game.window, render_loop, &game);
+	setup_hooks(&game);
 	render_frame(&game);
-	mlx_hook(game.window, 17, 0, close_game, &game);
 	mlx_loop(game.mlx);
 	cleanup_game(&game);
 	return (0);
